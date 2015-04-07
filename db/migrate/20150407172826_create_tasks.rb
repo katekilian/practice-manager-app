62c29c76b33c0775aeea5d860a6ee6e1d2795897
Kate Kilian Vovk <kathrynjkilian@gmail.com>
2015-04-07 11:48:08 -0600

    Added attributes to migration files and ran them; created models for all existing tables

--- db/migrate/20150407172826_create_tasks.rb
+++ db/migrate/20150407172826_create_tasks.rb
@@ -1,6 +1,10 @@
 class CreateTasks < ActiveRecord::Migration
   def change
     create_table :tasks do |t|
+      t.string  :description
+      t.date    :due_date
+      t.integer :category_id
+      t.timestamps
     end
   end
 end
